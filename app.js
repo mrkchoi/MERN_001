6a9ddeed225cba14642d4d42c857ee5f7301bb60
Kenneth Choi <kennethichoi@gmail.com>
2019-07-13 13:43:42 -0700

    Create initial UserSchema

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const tweets = require('./routes/api//tweets');
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
 
-  
+
 app.get('/', (req, res) => res.send("Hello World updated"));
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
